843067c702d21f3cddcddf5a5e725c93cc943673
tashaxanavsem82 <swgwull@hotmail.com>
2025-10-06 13:14:13 -0500

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
